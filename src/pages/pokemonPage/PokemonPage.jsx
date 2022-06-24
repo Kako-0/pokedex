360b36ff9f978b26169ec5f775e336dcff4cb475
Kayro França <k4yro01@gmail.com>
2022-06-24 20:17:58 -0300

    add search component on page

--- src/pages/pokemonPage/PokemonPage.jsx
+++ src/pages/pokemonPage/PokemonPage.jsx
@@ -1,9 +1,20 @@
+import SearchIcon from "../../common/assets/imgs/search.svg";
+import PokeCard from "../../components/pokeCard/PokeCard";
+import "./styles.scss";
+
 function PokemonPage() {
-    return (
-        <>
-            <div>Poke Page</div>
-        </>
-    )
+	return (
+		<main className="pokemonPageMainContainer">
+			<h1>Mais de 250 Pokemons para você escolher o seu favorito</h1>
+			<div className="searchInput">
+				<input type="search" placeholder="Pesquisar pokemon" />
+				<button>
+					<img src={SearchIcon} alt="lupaIcon" />
+				</button>
+			</div>
+			<section>{/*<PokeCard />*/}</section>
+		</main>
+	);
 }
 
 export default PokemonPage;
