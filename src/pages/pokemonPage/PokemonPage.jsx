1c3c87c56a8ed7d1bdfec934adfbac008094c119
Kayro França <kayro.dev@gmail.com>
2023-06-18 17:34:48 -0300

    Remove unused import

--- src/pages/pokemonPage/PokemonPage.jsx
+++ src/pages/pokemonPage/PokemonPage.jsx
@@ -1,20 +1,19 @@
 import SearchIcon from "../../common/assets/imgs/search.svg";
-import PokeCard from "../../components/pokeCard/PokeCard";
 import "./styles.scss";
 
 function PokemonPage() {
-	return (
-		<main className="pokemonPageMainContainer">
-			<h1>Mais de 250 Pokemons para você escolher o seu favorito</h1>
-			<div className="searchInput">
-				<input type="search" placeholder="Pesquisar pokemon" />
-				<button>
-					<img src={SearchIcon} alt="lupaIcon" />
-				</button>
-			</div>
-			<section>{/*<PokeCard />*/}</section>
-		</main>
-	);
+  return (
+    <main className="pokemonPageMainContainer">
+      <h1>Mais de 250 Pokemons para você escolher o seu favorito</h1>
+      <div className="searchInput">
+        <input type="search" placeholder="Pesquisar pokemon" />
+        <button>
+          <img src={SearchIcon} alt="lupaIcon" />
+        </button>
+      </div>
+      <section>{/*<PokeCard />*/}</section>
+    </main>
+  );
 }
 
 export default PokemonPage;
